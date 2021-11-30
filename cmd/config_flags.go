e07895e10c6136c38a4b107a6e797f8864bb1b9c
liquanhui01 <695762725@qq.com>
2021-11-30 11:58:47 +0800

    feat: Add static variables for cmd

---
+++ cmd/config_flags.go
@@ -0,0 +1,15 @@
+// Copyright 2021 Quanhui Li. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+
+package cmd
+
+// Defines flag for filestore.
+const (
+	FlagConfig      = "filestore"
+	FlagBearerToken = "user.token"
+	FlagUsername    = "user.username"
+	FlagPassword    = "user.password"
+	FlagSecureID    = "user.secure_id"
+	FlagSecretKey   = "user.secret_key"
+)
