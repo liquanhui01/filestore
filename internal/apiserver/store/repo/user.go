c2fbf1cb90ee01b99aee1459c654401ef5e2c30b
liquanhui01 <695762725@qq.com>
2021-11-30 16:42:13 +0800

    fix: Modify the structure for table user.

--- internal/apiserver/store/repo/user.go
+++ internal/apiserver/store/repo/user.go
@@ -6,8 +6,8 @@ type User struct {
 	gorm.Model
 	Username string `gorm:"not null;unique;comment:'用户名'" json:"user_name" validate:"required,min=2,max=30"`
 	Password string `gorm:"not null;comment:'用户密码'" json:"password" validate:"required,min=6,max=20"`
-	Email    string `gorm:"default:'';unique;comment:'邮箱'" json:"email" validate:"omitempty"`
-	Phone    string `gorm:"default:'';unique;comment:'联系方式'" json:"phone" validate:"omitempty"`
+	Email    string `gorm:"default:'';comment:'邮箱'" json:"email"`
+	Phone    string `gorm:"default:'';comment:'联系方式'" json:"phone"`
 	Avatar   string `gorm:"default:'';comment:'用户头像'" json:"avatar" validate:"omitempty"`
 	Profile  string `gorm:"default:'';comment:'用户简介'" json:"profile" validate:"omitempty"`
 	Status   int64  `gorm:"comment:'0表示启用，1表示禁用，2表示锁定，3表示标记删除'" json:"status" validate:"omitempty"`
--- pkg/db/sql/user.sql
+++ pkg/db/sql/user.sql
@@ -5,9 +5,10 @@ USE filestore;
 DROP TABLE `user` IF EXISTS;
 CREATE TABLE `user` (
     `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT "用户id",
-    `user_name` VARCHAR(50) UNIQUE NOT NULL DEFAULT '' COMMENT "用户名",
-    `email` varchar(50) UNIQUE NULL DEFAULT '' COMMENT "用户邮箱",
-    `phone` varchar(30) UNIQUE NULL DEFAULT '' COMMENT "联系方式",
+    `username` VARCHAR(50) UNIQUE NOT NULL COMMENT "用户名",
+    `password` VARCHAR(100) NOT NULL COMMENT "用户密码",
+    `email` VARCHAR(50) NULL DEFAULT '' COMMENT "用户邮箱",
+    `phone` VARCHAR(30) NULL DEFAULT '' COMMENT "联系方式",
     `avatar` Text NULL COMMENT "用户头像",
     `profile` Text NULL COMMENT "用户简介",
     `status` SMALLINT NOT NULL COMMENT "用户状态, 0表示启用，1表示禁用，2表示锁定，3表示标记删除",
